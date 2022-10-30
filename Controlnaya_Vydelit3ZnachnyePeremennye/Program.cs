beb15f7e1d82510ab3619b56f142f515b97737c0
Alexey <manoil77@list.ru>
2022-10-30 09:57:43 +0300

    Add entering array data

--- Controlnaya_Vydelit3ZnachnyePeremennye/Program.cs
+++ Controlnaya_Vydelit3ZnachnyePeremennye/Program.cs
@@ -5,3 +5,26 @@
 // ["Hello", "2", "world", ":-)"] -> [":-)"]
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+string str;
+int count = 0; 
+string[] ArrStr = new string[count]; 
+string[] ArrStr2 = new string[count];
+string[] ArrResult = new string[count];
+
+Console.WriteLine("Введите строки : ");
+do
+{
+    str = Console.ReadLine();
+    if (str!="")
+    {
+        count++;
+        ArrStr2 = new string[count];
+
+        for (int i = 0; i < ArrStr2.Length - 1; i++)
+        ArrStr2[i] = ArrStr[i];
+
+        ArrStr2[count - 1] = str;
+        ArrStr = ArrStr2;
+    }
+} while (str != "");
